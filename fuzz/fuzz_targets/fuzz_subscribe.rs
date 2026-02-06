222834e10354057ce4e381e4d0428009ae5e5a1e
Auke Oosterhoff <auke@orangetux.nl>
2026-02-06 15:55:17 +0100

    fixup! fixup! fixup! GH-140 Replace `Filters` in favor of `Vec<(Filter<'_>, QoS)>'`

--- fuzz/fuzz_targets/fuzz_subscribe.rs
+++ fuzz/fuzz_targets/fuzz_subscribe.rs
@@ -15,6 +15,5 @@ fuzz_target!(|data: Builder| {
     // All these calls expect a correct packet. If packet is incorrect,
     // the calls cause panic.
     subscribe_1.packet_identifier();
-    let topics = subscribe_1.topics();
-    for _ in topics {}
+    subscribe_1.filters();
 });
